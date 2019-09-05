19a7da1962d4fe0a33f3901c09aa37b4ed6b3661
Oleg Brutich <oleg87arh@gmail.com>
2019-09-05 23:22:24 +0300

    Added Option class

--- src/RegEx/Regex.cs
+++ src/RegEx/Regex.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using System.Text;
 using Dynamo.Graph.Nodes;
 using CSRegex = System.Text.RegularExpressions.Regex;
+using CSRegexOptions = System.Text.RegularExpressions.RegexOptions;
 
 
 namespace RegularExpressions
@@ -14,21 +15,40 @@ namespace RegularExpressions
     /// </summary>
     public class Regex
     {
-        private CSRegex Pattern { get; set; }
-        
+
+        private CSRegex Pattern { get; set; }        
+
         private Regex(String pattern)
         {
             Pattern = new CSRegex(pattern);
+        }
+
+        private Regex(String pattern, CSRegexOptions options)
+        {
+            Pattern = new CSRegex(pattern, options);
         }
 
         /// <summary>
         ///     Regular expression by pattern.
         /// </summary>
         /// <param name="pattern"></param>
+        /// <param name="options"></param>
         [NodeCategory("Create")]
-        public static Regex ByPattern(String pattern)
+        public static Regex ByPattern(string pattern, Option[] options)
         {
-            return new Regex(pattern);
+            if (pattern == null)
+                return null;
+
+            if (options == null | !options.Any())
+                return new Regex(pattern);
+
+            CSRegexOptions regexOptions = options.First().RegexOption;
+            options = options.Skip(1).ToArray();
+
+            foreach (var opt in options)
+                regexOptions = regexOptions | opt.RegexOption;
+
+            return new Regex(pattern, regexOptions);
         }
 
 
@@ -83,6 +103,56 @@ namespace RegularExpressions
         public static string[] Split(Regex regex, String input)
         {
             return regex.Pattern.Split(input);
+        }
+    }
+
+
+    /// <summary>
+    /// Option for regular expression.
+    /// </summary>
+    public class Option
+    {
+        internal CSRegexOptions RegexOption { get; set; }
+
+        private Option(CSRegexOptions option)
+        {
+            RegexOption = option;
+        }
+
+
+        /// <summary>
+        /// Create regular expression option by name.
+        /// </summary>
+        /// <param name="optionName"></param>
+        /// <returns></returns>
+        [NodeCategory("Create")]
+        public static Option ByName(String optionName)
+        {
+            switch (optionName)
+            {
+                case "IgnoreCase":
+                    return new Option(CSRegexOptions.IgnoreCase);
+                case "Singleline":
+                    return new Option(CSRegexOptions.Singleline);
+                case "Multiline":
+                    return new Option(CSRegexOptions.Multiline);
+                case "Compiled":
+                    return new Option(CSRegexOptions.Compiled);
+                case "ExplicitCapture":
+                    return new Option(CSRegexOptions.ExplicitCapture);
+                case "IgnorePatternWhitespace":
+                    return new Option(CSRegexOptions.IgnorePatternWhitespace);
+                case "CultureInvariant":
+                    return new Option(CSRegexOptions.CultureInvariant);
+                case "ECMAScript":
+                    return new Option(CSRegexOptions.ECMAScript);
+                case "RightToLeft":
+                    return new Option(CSRegexOptions.RightToLeft);
+                case "None":
+                    return new Option(CSRegexOptions.None);
+                default:
+                    throw new FormatException("Name is incorrect.");
+            }
         }
     }
 }
